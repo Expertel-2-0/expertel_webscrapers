6254c841447101457d5005521ded82e4596fd7a0
Alejandro <alejandro.granados.dev@gmail.com>
2026-04-11 14:35:40 -0600

    clean filter

--- web_scrapers/application/scraper_job_service.py
+++ web_scrapers/application/scraper_job_service.py
@@ -98,7 +98,7 @@ class ScraperJobService:
 
         # Build base query filter for PENDING jobs
         query_filter = Q(status=ScraperJobStatus.PENDING)
-        query_filter &= Q(scraper_config_id__in=(5,))
+
 
         if include_null_available_at:
             query_filter &= Q(available_at__lte=current_time) | Q(available_at__isnull=True)
